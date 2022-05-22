975c688c57ee475b0aea67e88623ffeda285025e
ElNov686 <elena5966286@gmail.com>
2022-05-22 18:47:10 +0300

    Second commit

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -25,7 +25,8 @@ public class WebTest {
         menuBrowsLanguages.click();
 
         WebElement menuStart = driver.findElement(By.xpath(
-                        "//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/']")
+
+                "//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/']")
         );
         menuStart.click();
 
@@ -38,6 +39,88 @@ public class WebTest {
 
         driver.quit();
 
+    }
+
+    @Test
+    public void testHeaderH1() throws InterruptedException {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Users/elena/Downloads/chromedriver";
+        String url = "http://www.99-bottles-of-beer.net/";
+        String expectedResult = "99 Bottles of Beer";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+
+        WebElement headerH1 = driver.findElement(By.xpath(
+                "//body/div[@id='wrap']/div[@id='header']/h1")
+        );
+        headerH1.click();
+
+        String actualResult = headerH1.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        driver.quit();
 
     }
-}
+
+    @Test
+    public void testSubmitNewLanguage() throws InterruptedException {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Users/elena/Downloads/chromedriver";
+        String url = "http://www.99-bottles-of-beer.net/";
+        String expectedResult = "Submit new Language";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+
+        WebElement menuSubmitNewLanguage = driver.findElement(By.xpath(
+                "//body/div[@id='wrap']/div[@id='header']/div[@id='navigation']/ul[@id='menu']" +
+                        "li[@class='sel']/a[@href='/submitnewlanguage.html']")
+        );
+        menuSubmitNewLanguage.click();
+
+        String actualResult = menuSubmitNewLanguage.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        driver.quit();
+    }
+    //1. Открыть вебсайт на странице
+    //2. Считать название первого подзаголовка
+    //3. Подтвердить, что название подменю соответствует ожидаемому
+    //4. Закрыть браузер
+    @Test
+    public void testsubMenuCategoryA() throws InterruptedException {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Users/elena/Downloads/chromedriver";
+        String url = "http://www.99-bottles-of-beer.net/a.html";
+        String expectedResult = "Category A";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+
+        WebElement menuSubmitNewLanguage = driver.findElement(By.xpath(
+                "//body/div[@id='wrap']/div[@id='main']/h2")
+        );
+        menuSubmitNewLanguage.click();
+
+        String actualResult = menuSubmitNewLanguage.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        driver.quit();
+    }
+    //TC_11_04
+    //1. Открыть вебсайт на странице: http://www.99-bottles-of-beer.net/abc. html
+    //2. Считать название первого подзаголовка
+    //3. Подтвердить, что название подменю соответствует ожидаемому 0-9
+    //4. Закрыть браузер
+
+    }
