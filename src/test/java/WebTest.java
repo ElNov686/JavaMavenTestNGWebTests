2b1d026c1505fce8d330c3d9cfb6ba268ac83875
ElNov686 <elena5966286@gmail.com>
2022-05-27 21:01:01 +0100

    testTeamMembers() created

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -65,7 +65,7 @@ public class WebTest {
         driver.quit();
 
     }
-
+    //TC_11_03
     @Test
     public void testSubmitNewLanguage() throws InterruptedException {
         String chromeDriver = "webdriver.chrome.driver";
@@ -122,5 +122,68 @@ public class WebTest {
     //2. Считать название первого подзаголовка
     //3. Подтвердить, что название подменю соответствует ожидаемому 0-9
     //4. Закрыть браузер
+    @Test
+    public void testSubmenu0and9() throws InterruptedException {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Users/elena/Downloads/chromedriver";
+        String url = "http://www.99-bottles-of-beer.net/a.html";
+        String expectedResult = "0-9";
 
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+
+        WebElement menuSubmenu9and0 = driver.findElement(By.xpath(
+                "//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a")
+        );
+
+        String actualResult = menuSubmenu9and0.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        driver.quit();
+    }
+    //TC_11_06 Подтвердите, что имена создателей сайта:
+    //Oliver Schade
+    //Gregor Scheithauer
+    //Stefan Scheler
+
+    //1. Открыть вебсайт на главной странице
+    //2. Кликнуть на ссылку  team members
+    //3. Подтвердить, имена создателей соответствует ожидаемому
+    //4. Закрыть браузер
+
+    @Test
+    public void testTeamMembers() throws InterruptedException {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Users/elena/Downloads/chromedriver";
+        String url = "http://www.99-bottles-of-beer.net/team.html";
+        String [] expectedResult = {"Oliver Schade", "Gregor Scheithauer", "Stefan Scheler"};
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+
+        driver.findElement(By.xpath("//a[@href='team.html']"))
+                .click();
+
+        String [] actualResult = new String[3];
+        WebElement firstCreatorName = driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/h3[text()='Oliver Schade']"));
+        actualResult[0] = firstCreatorName.getText();
+        Assert.assertEquals(actualResult[0], expectedResult[0]);
+
+        WebElement secondCreatorName = driver.findElement(By.xpath(
+                "//body/div[@id='wrap']/div[@id='main']/h3[text()='Gregor Scheithauer']"));
+        actualResult[1] = secondCreatorName.getText();
+        Assert.assertEquals(actualResult[1], expectedResult[1]);
+
+        WebElement thirdCreatorName = driver.findElement(By.xpath(
+                "//body/div[@id='wrap']/div[@id='main']/h3[text()='Stefan Scheler']"));
+        actualResult[2] = thirdCreatorName.getText();
+        Assert.assertEquals(actualResult[2], expectedResult[2]);
+
+        driver.quit();
+    }
     }
